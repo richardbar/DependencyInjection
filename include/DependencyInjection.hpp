2ad92a1fd848e44c97069c3097ca6bd82c71fd33
Rikarnto Bariampa <rikarntobariampa@outlook.com>
2023-09-30 01:18:04 +0300

    Added AddSingleton with factory declaration in ServiceCollection.

--- include/DependencyInjection.hpp
+++ include/DependencyInjection.hpp
@@ -1,18 +1,18 @@
 /**
  * MIT License
- * 
+ *
  * Copyright (c) 2023 Rikarnto Bariampa
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -111,6 +111,9 @@ namespace DependencyInjection
 
             template<class TService, class TImplementation = TService>
             ServiceCollection& AddSingleton();
+
+            template<class TService, class TImplementation = TService>
+            ServiceCollection& AddSingleton(const DependencyInjection::ServiceFactory& factory);
     };
 }
 
@@ -180,6 +183,14 @@ DependencyInjection::IServiceCollection& DependencyInjection::ServiceCollection:
 template<class TService, class TImplementation>
 DependencyInjection::ServiceCollection& DependencyInjection::ServiceCollection::AddSingleton()
 {
+}
+
+template<class TService, class TImplementation>
+DependencyInjection::ServiceCollection& DependencyInjection::ServiceCollection::AddSingleton(const DependencyInjection::ServiceFactory& factory)
+{
+    auto serviceDescriptor = DependencyInjection::ServiceDescriptor(typeid(TService), factory, DependencyInjection::ServiceLifetime::Singleton);
+
+    return this->Add(serviceDescriptor);
 }
 
 #endif
